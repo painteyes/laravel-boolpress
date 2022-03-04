a60543329f0bdfb54e55499da27d114052932b07
painteyes <andreantoniocchipinti@gmail.com>
2022-03-04 15:39:57 +0100

    add single post api

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -9,11 +9,29 @@ use App\Post;
 class PostController extends Controller
 {
     public function index() {
+        
         $posts = Post::all();
 
         return response()->json([
             'success'=> true, 
             'result' => $posts
         ]);
+    }
+
+    public function show($slug) {
+
+        $post = Post::where('slug', '=', $slug)->first();
+
+        if($post) {
+            return response()->json([
+                'success'=> true, 
+                'result' => $post 
+            ]);
+        } else {
+            return response()->json([
+                'success'=> false, 
+                'result' => [] 
+            ]); 
+        }
     }
 }
